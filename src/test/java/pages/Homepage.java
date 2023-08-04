757c0d01826a9a8730cc26391cdf52865ffd2474
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2023-08-03 23:13:36 -0500

    tc03

--- src/test/java/pages/Homepage.java
+++ src/test/java/pages/Homepage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
+import java.util.List;
+
 public class Homepage {
     public Homepage (){
         PageFactory.initElements(Driver.getDriver(),this);
@@ -15,6 +17,29 @@ public class Homepage {
     @FindBy(linkText = "Home")
     //@FindBy(xpath = "//a[.='Home']")
     public WebElement homeButton;
+    @FindBy (xpath = "//*[@id=\"themify_builder_content-22\"]/div[2]/div/div/div/div/div[2]")
+    public WebElement threeSliders;
+
+    @FindBy(xpath = "//div[@data-slide-duration='0']")
+    public List<WebElement> threeSlidersOnly;
+
+    @FindBy(xpath = "//div[@class='themify_builder_sub_row clearfix gutter-default   sub_row_1-0-2']/div")
+    public List<WebElement> threeArrivalOnly;
+
+    @FindBy(xpath = "//div[@class='themify_builder_sub_row clearfix gutter-default   sub_row_1-0-2']/div[1]//img")
+    public WebElement image1;
+
+    @FindBy(xpath = "//button[.='Add to basket']")
+    public WebElement addToBasketButton;
+    @FindBy(xpath = "//div[@class='woocommerce-message']")
+    public WebElement messageText;
+
+    @FindBy(xpath = "//div[@class='themify_builder_sub_row clearfix gutter-default   sub_row_1-0-2']/div[1]//h3")
+    public WebElement itemInImage1;
+
+    @FindBy(xpath = "//div[@class='themify_builder_sub_row clearfix gutter-default   sub_row_1-0-2']/div[3]//h3")
+    public WebElement itemInImage3;
+
 
 
 
